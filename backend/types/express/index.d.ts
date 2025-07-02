3fd056a55e478129f575495cbd628201b23423d3
winnyineza <w.ineza@alustudent.com>
2025-07-02 12:13:56 +0200

    removed all

--- backend/types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import 'express';
-
-// Extend Express Request type to include 'user' property
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: any;
-  }
-} 
--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './mobile-app/App';
-import {name as appName} from './package.json';
-
-AppRegistry.registerComponent(appName, () => App);
